244ad36f2f71452bf4bd41dedc1ff56dc9d46aa3
Utkarsh Sharma <utkarshkviim@gmail.com>
2022-10-21 23:33:34 +0530

    Time: 1 ms (99.90%), Space: 54.3 MB (66.55%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,7 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
+        //initialize length create
         int length = s.length;
         for(int i=0; i<length/2; i++){
+            
+            //swap fist and last element and 
             char tmp = s[length-i-1];
             s[length-i-1] = s[i]; 
             s[i] = tmp;
